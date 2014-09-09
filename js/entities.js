4d9c91ae44d675cab389ac10b6acae89e4049316
SweSnow <swesnowme@gmail.com>
2014-09-09 13:19:24 +0200

    minor

--- js/entities.js
+++ js/entities.js
@@ -74,72 +74,64 @@ function spawnRandomFood(logLastSpawn) {
 }
 
 function determineSpawnRandomBug() {
-	if (Math.random() > 0.5) {
-		//50% chance
 
-		var proposedX = Math.floor(Math.random() * gameOptions.canvasWidth / foodSize) * foodSize;
-		var proposedY = Math.floor(Math.random() * gameOptions.canvasWidth / foodSize) * foodSize;
+	var proposedX = Math.floor(Math.random() * gameOptions.canvasWidth / foodSize) * foodSize;
+	var proposedY = Math.floor(Math.random() * gameOptions.canvasWidth / foodSize) * foodSize;
 
-		var canSpawn = true;
+	var canSpawn = true;
 
-
-
-		for (var i = 0; i < foodArray.length; i++) {
-			if (foodArray[i].x == proposedX && foodArray[i].y == proposedY) {
-				canSpawn = false;
-			}
+	for (var i = 0; i < foodArray.length; i++) {
+		if (foodArray[i].x == proposedX && foodArray[i].y == proposedY) {
+			canSpawn = false;
 		}
-		
-		for (var i = 0; i < bugArray.length; i++) {
-			if (bugArray[i].x == proposedX && bugArray[i].y == proposedY) {
-				canSpawn = false;
-			}
-		}
-
-		for (var i = 0; i < tailLength; i++) {
-			if (tailArray[tailArray.length - i - 1].x == proposedX && tailArray[tailArray.length - i - 1].y == proposedY) {
-				canSpawn = false;
-			}
-		}
-
-		if (gameOptions.gameMode == gameModes['obstacle']) {
-			for (var yi = 0; yi < 50; yi++) {
-				for (var xi = 0; xi < 50; xi++) {
-					if (proposedX == xi && proposedY == yi) {
-						canSpawn = false;
-					}
-				}
-			}
-		}
+	}
 	
-		if (canSpawn) {
+	for (var i = 0; i < bugArray.length; i++) {
+		if (bugArray[i].x == proposedX && bugArray[i].y == proposedY) {
+			canSpawn = false;
+		}
+	}
 
-			lastBugSpawn = Date.now();
+	for (var i = 0; i < tailLength; i++) {
+		if (tailArray[tailArray.length - i - 1].x == proposedX && tailArray[tailArray.length - i - 1].y == proposedY) {
+			canSpawn = false;
+		}
+	}
 
-			var bug = {
-				x: proposedX,
-				y: proposedY,
-				time: Date.now()
-			};
-
-			bugArray.push(bug);
-
-			writeLogMessage('Spawned bug at (' + proposedX + ', ' + proposedY + ')');
-
-			setTimeout(function() {
-			for (var i = 0; i < bugArray.length; i++) {
-				if (bugArray[i] == bug) {
-					bugArray.splice(i, 1);
+	if (gameOptions.gameMode == gameModes['obstacle']) {
+		for (var yi = 0; yi < 50; yi++) {
+			for (var xi = 0; xi < 50; xi++) {
+				if (proposedX == xi && proposedY == yi) {
+					canSpawn = false;
 				}
 			}
-		}, 7000);
-
-		} else {
-			determineSpawnRandomBug();
 		}
+	}
+
+	if (canSpawn) {
+
+		lastBugSpawn = Date.now();
+
+		var bug = {
+			x: proposedX,
+			y: proposedY,
+			time: Date.now()
+		};
+
+		bugArray.push(bug);
+
+		writeLogMessage('Spawned bug at (' + proposedX + ', ' + proposedY + ')');
+
+		setTimeout(function() {
+		for (var i = 0; i < bugArray.length; i++) {
+			if (bugArray[i] == bug) {
+				bugArray.splice(i, 1);
+			}
+		}
+	}, 7000);
 
 	} else {
-		writeLogMessage('Bug spawn denied');
+		determineSpawnRandomBug();
 	}
 }
 
