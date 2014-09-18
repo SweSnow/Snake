6de2ef45e3891f623a52bb7677673948594d781f
John Rapp <johnfarnes@hotmail.com>
2014-09-18 21:01:32 +0200

    Some client side work

--- server.js
+++ server.js
@@ -6,3 +6,7 @@ app.use(express.static(__dirname));
 app.listen(8088, function() {
 	console.log('Server up on port 8088.')
 });
+
+app.get('*', function(req, res) {
+	res.sendfile(__dirname + '/index.html');
+});
