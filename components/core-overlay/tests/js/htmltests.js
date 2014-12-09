731aaece15ef38116afdcd180bc4ee18fa7775ff
John Rapp <johnfarnes@hotmail.com>
2014-12-09 11:24:39 +0100

    Updated hacked system

--- components/core-overlay/tests/js/htmltests.js
+++ components/core-overlay/tests/js/htmltests.js
@@ -2,4 +2,6 @@ htmlSuite('core-overlay', function() {
   htmlTest('html/core-overlay-basic.html');
   htmlTest('html/core-overlay-positioning.html');
   htmlTest('html/core-overlay-positioning-margin.html');
+  htmlTest('html/core-overlay-scroll.html');
+  htmlTest('html/core-overlay-quick-close.html');
 });
